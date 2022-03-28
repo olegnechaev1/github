62d9a369cd82c2450865aa2c81182b48186cf793
olegnechaev1 <101923197+olegnechaev1@users.noreply.github.com>
2022-03-28 15:59:49 +0300

    Update TMS task7.py

--- task7/TMS task7.py
+++ task7/TMS task7.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-def my_decor (func):
+def function_description(func):
     """ Декоратор показывает время выполнения функции,количество аргументов и их соотвнтственно """
     def wrapper(*args,**kwargs) :
         start = datetime.now()
@@ -9,22 +9,22 @@ def my_decor (func):
         print(f"Время выполнения функции : {diff.total_seconds()/60}")
         func_1 = len(args) + len(kwargs)
         print(f"Количество аргументов : {func_1}")
-        argss = [repr(arg)for arg in args]
-        kwargss = [repr(kwarg)for kwarg in kwargs]
+        argss = [repr(arg) for arg in args]
+        kwargss = [repr(kwarg) for kwarg in kwargs]
         func_2 = ",".join(argss+kwargss)
         print(f"Аргументы в функции : {func_2} ")
  #не мог красиво вывести результат через str,поэтому пришлось почитать и применить repr и join       
     return  wrapper    
-@my_decor                 
-def names (names,username):
-    return f"hello {names} {username}" 
-names("oleg","nechaev")
-@my_decor
-def num (a, b, c):
+@function_description                 
+def personal_data(names,surname: str)-> str:
+    return f"hello {names} {surname}" 
+personal_data("oleg","nechaev")
+@function_description
+def num (a, b, c: int)-> int:
     return a + b + c
 num(2, 5, 8)
 
-print(my_decor.__doc__ )
+print(function_description.__doc__ )
 
 my_list = [-1, 20, -80, -10, 66, 70, -19, 33, 34, 35, 66 ,-77]
 my_list = list(filter(lambda x : x < 0 , my_list))
