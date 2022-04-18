262c02d78dad0557209b5c220a971efe85da1281
olegnechaev1 <nechaev20000@gmail.com>
2022-04-19 00:35:51 +0300

    change task12

--- task12/tms12.py
+++ task12/tms12.py
@@ -1,12 +1,13 @@
 import re
 def geometric_progression(n):
-   while True:
-       yield n
-       n *= 2
-test = geometric_progression(1)
+       while range(n,10000):
+           for i in range(n,10000):
+                if i >1000:
+                    break
+           yield n
+           n *= 2
+test = geometric_progression(2)
 for i in test:
-    if i > 10000:
-        break
     print(i)
 
 
