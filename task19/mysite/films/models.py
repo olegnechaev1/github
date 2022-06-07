8120c3ddff464f5911e4e6f605a241d16f318834
olegnechaev1 <nechaev20000@gmail.com>
2022-06-07 14:17:55 +0300

    fix models.py

--- task19/mysite/films/models.py
+++ task19/mysite/films/models.py
@@ -20,14 +20,14 @@ class Film(models.Model):
     author = models.ForeignKey('films.User', on_delete=models.CASCADE, related_name='authors',blank=True, null=True)
     comment = models.TextField(blank=True, null=True)
     date = models.DateTimeField(auto_now=True)
-    liked = models.ManyToManyField('films.User', default=None, blank=True, related_name='liked')
+    liked = models.ManyToManyField('films.User', blank=True, related_name='liked')
     
     def __str__(self):
         return f'Film comment:{self.comment}, Film date:{self.date}'
     
     @property
     def num_likes(self):
-        return str(self.liked.all().count())
+        return self.liked.all().count()
     
 LIKE_CHOICES = (
     ('Like', 'Like'),
